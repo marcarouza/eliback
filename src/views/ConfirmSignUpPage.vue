71f8c13fe7ee96a55661623579bb494a3337c8a1
Eli Azoura <eliazoura@gmail.com>
2024-08-08 10:22:20 +0200

    fix du nom ConfirmSignUp pour sa page

--- src/views/ConfirmSignUpPage.vue
+++ src/views/ConfirmSignUpPage.vue
@@ -1,18 +1,33 @@
 <!-- src/components/ConfirmationPage.vue -->
-<template></template>
+<template>
+	<div>
+		<NavOk />
+		<ConfirmSignUp>
+			<TabWhats />
+			<Chat_Box />
+		</ConfirmSignUp>
+		<Footer />
+	</div>
+</template>
 
 <script>
-import ConfirmSignUp from './ConfirmSignUp.vue';
+import ConfirmSignUp from '../components/ConfirmSignUp.vue';
+import NavOk from '../components/NavOk.vue';
+import Footer from '../components/Footer.vue';
+import Chat_Box from '../components/Chat_Box.vue';
+import TabWhats from '../components/TabWhats.vue';
 
 export default {
 	name: 'ConfirmSignUpPage',
-	emits: ['updatePageTitle'], // Déclarez l'événement ici
-	// Autres options du composant
+	emits: ['updatePageTitle'],
+	components: {
+		NavOk,
+		ConfirmSignUp,
+		Chat_Box,
+		TabWhats,
+		Footer,
+	},
 };
 </script>
 
-<style scoped>
-.container {
-	margin-top: 50px;
-}
-</style>
+<style scoped></style>
