d2292122ad6aa6715ae22b6f792749227988ebcc
Eli Azoura <eliazoura@gmail.com>
2024-08-19 12:44:42 +0200

    bis

--- src/components/blogCompo/PostDetail.vue
+++ src/components/blogCompo/PostDetail.vue
@@ -1,9 +1,13 @@
 <template>
-	<div class="post-detail">
-		<h1>{{ post.title }}</h1>
+	<div v-if="post" class="post-detail">
+		<h1>Titre &nbsp;{{ post.title }}</h1>
 		<p>Par {{ post.author }}</p>
 		<p>{{ post.content }}</p>
 		<p>Publié le {{ formatDate(post.createdAt) }}</p>
+	</div>
+
+	<div v-else class="nopost">
+		<p>Désolé, un poblème technique est survenu</p>
 	</div>
 </template>
 
@@ -15,27 +19,35 @@ export default {
 			post: null,
 		};
 	},
-	async created() {
-		const postId = this.$route.params.id;
-		try {
-			const response = await fetch(
-				`https://eli-back.onrender.com/api/onePostById/${postId}`
-			);
-			if (response.ok) {
-				this.post = await response.json();
-			} else {
-				console.error(
-					"Erreur lors de la récupération de l'article."
-				);
-			}
-		} catch (error) {
-			console.error(
-				"Erreur réseau lors de la récupération de l'article:",
-				error
-			);
-		}
+	created() {
+		this.fetchOnePost();
 	},
 	methods: {
+		async fetchOnePost() {
+			const postId = this.$route.params.id;
+			console.log('✅ ~ FROM fetchOnePost => postId :', postId);
+			try {
+				const response = await fetch(
+					`https://eli-back.onrender.com/api/onePostById/${postId}`
+				);
+				if (response.ok) {
+					this.post = await response.json();
+					console.log(
+						'🚀 ~ fetchOnePost ~ this.post :',
+						this.post
+					);
+				} else {
+					console.error(
+						"🍌 Erreur lors de la récupération de l'article."
+					);
+				}
+			} catch (error) {
+				console.error(
+					'🍌 Erreur CTACH GLBAL lors du FETCH réseau  de récupération du POST : ',
+					error
+				);
+			}
+		},
 		formatDate(date) {
 			return new Date(date).toLocaleDateString();
 		},
